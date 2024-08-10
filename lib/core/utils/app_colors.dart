d401b955a7644b38cd360d320da17829d157f832
ahmedhossam1212 <ahossamm404@gmail.com>
2024-08-10 16:38:27 +0300

    create app architecture

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:coffee_dashboard/core/utils/hex_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color white = HexColor("FFFFFF");
+  static Color offWhite = HexColor("#F9F2ED");
+  static Color black = HexColor("#313131");
+  static Color lightGrey = HexColor("#E3E3E3");
+  static Color nude = HexColor("#EDD6C8");
+  static Color brown = HexColor("#C67C4E");
+}
