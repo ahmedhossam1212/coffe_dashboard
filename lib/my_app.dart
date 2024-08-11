9eba2566430a7dcd0f5f64d023f00f90a48a78a3
ahmedhossam1212 <ahossamm404@gmail.com>
2024-08-11 20:07:12 +0300

    add localization

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,7 @@
 import 'package:coffee_dashboard/config/routes/app_routes.dart';
+import 'package:coffee_dashboard/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,6 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
     );
